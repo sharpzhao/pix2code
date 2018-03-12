794ad8e4219e344b48af73b37ea361a1782c9704
sharpzhao <sharpzhao@hotmail.com>
2018-03-11 21:45:30 -0700

    UDPATE:

--- plot_benchmark.sh
+++ plot_benchmark.sh
@@ -13,5 +13,5 @@ case $arg in
 	;;
         esac
 done
-toplev.py -I 1000 -l3 -x, -o tl-build.csv ~/pix2code/run.sh -i ${instructions} -t ${thread}
-tl-barplot.py --cpu C0-T0 tl-build.csv -o ~/plot_${instruction}_${thread}.png
+/root/pmu-tools/toplev.py -I 1000 -l3 -x, -o tl-build.csv ~/pix2code/run.sh -i ${instructions} -t ${thread}
+/root/pmu-tools/tl-barplot.py --cpu C0-T0 tl-build.csv -o ~/plot_${instruction}_${thread}.png
